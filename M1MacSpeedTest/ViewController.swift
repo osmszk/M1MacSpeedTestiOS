16ceeb1689d10e5ae9b950d5e59ab7f708227b6d
osuzuki <osuzuki@plegineer.com>
2020-12-25 08:00:02 +0900

    Initial Commit

---
+++ M1MacSpeedTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M1MacSpeedTest
+//
+//  Created by Osamu Suzuki on 2020/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
